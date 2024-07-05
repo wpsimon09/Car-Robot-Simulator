9324cd375edb55c6446739dd08e6fb2ac76e8748
wpsimon09 <simon.potocnak03@gmail.com>
2024-07-05 04:14:31 +0200

    made coherent

--- src/Engine/speechMain.ts
+++ src/Engine/speechMain.ts
@@ -6,342 +6,318 @@ import PathConstructor from './pathContructor';
 
 import Car from './car';
 
-  
+
 
 export default class Application {
 
-private _scene: THREE.Scene;
+    private _scene: THREE.Scene;
 
-private _renderer: THREE.Renderer;
+    private _renderer: THREE.Renderer;
 
-private _camera: THREE.PerspectiveCamera;
+    private _camera: THREE.PerspectiveCamera;
 
-private _controls: MapControls;
+    private _controls: MapControls;
 
-  
 
-private _car: Car;
 
-  
+    private _car: Car;
 
-private _mousePosX = 0;
 
-private _mousePosY = 0;
 
-  
+    private _mousePosX = 0;
 
-private _mousePointInWorld = new THREE.Vector3;
+    private _mousePosY = 0;
 
-  
 
-private _planeOfIntersection: THREE.Mesh;
 
-  
+    private _mousePointInWorld = new THREE.Vector3;
 
-// private _testObject: THREE.Mesh;
 
-  
 
-private _pathConstructor: PathConstructor
+    private _planeOfIntersection: THREE.Mesh;
 
-  
 
-private _isValidIntersection = false;
 
-private _stratButton;
+    // private _testObject: THREE.Mesh;
 
-  
 
-public constructor(_canvas: any) {
 
-this._scene = new THREE.Scene();
+    private _pathConstructor: PathConstructor
 
-this._renderer = new THREE.WebGLRenderer({ canvas: _canvas, antialias: true , alpha:true});
 
-this._renderer.setSize(_canvas.clientWidth, _canvas.clientHeight);
 
-  
+    private _isValidIntersection = false;
 
-this._camera = new THREE.PerspectiveCamera(65, _canvas.clientWidth / _canvas.clientHeight, 0.1, 1000.0);
+    private _stratButton;
 
-this._controls = new MapControls(this._camera, _canvas);
 
-  
 
-// creating car i guess
+    public constructor(_canvas: any) {
 
-this._car = new Car(this._scene)
+        this._scene = new THREE.Scene();
 
-this._processMouseMove = this._processMouseMove.bind(this);
+        this._renderer = new THREE.WebGLRenderer({ canvas: _canvas, antialias: true, alpha: true });
 
-this._processMouseClick = this._processMouseClick.bind(this);
+        this._renderer.setSize(_canvas.clientWidth, _canvas.clientHeight);
 
-this._start = this._start.bind(this);
 
-  
 
-// this._pathConstructor = new PathConstructor(this._scene);
+        this._camera = new THREE.PerspectiveCamera(65, _canvas.clientWidth / _canvas.clientHeight, 0.1, 1000.0);
 
-this._stratButton = document.getElementById("start")?.addEventListener('click', this._start)
+        this._controls = new MapControls(this._camera, _canvas);
 
-  
 
-this._renderer.domElement.addEventListener("mousemove", this._processMouseMove);
 
-// this._renderer.domElement.addEventListener("click", this._processMouseClick);
+        // creating car i guess
 
-  
+        this._car = new Car(this._scene)
 
-}
+        this._processMouseMove = this._processMouseMove.bind(this);
 
-  
+        this._processMouseClick = this._processMouseClick.bind(this);
 
-public init(orders: String[]): void {
+        this._start = this._start.bind(this);
 
-this._car.setOrders(orders)
 
-this._setUpLight();
 
-this._setUpFloor();
+        // this._pathConstructor = new PathConstructor(this._scene);
 
-this._setUpBox();
+        this._stratButton = document.getElementById("start")?.addEventListener('click', this._start)
 
-this._camera.position.set(0, 5, 5); // Position the camera above the plane
 
-this._camera.lookAt(0, 0, 0); // Look at the center of the plane
 
-  
+        this._renderer.domElement.addEventListener("mousemove", this._processMouseMove);
 
-this._controls.target.set(0, 0, 0); // Set the target for the controls to the center of the plane
+        // this._renderer.domElement.addEventListener("click", this._processMouseClick);
 
-  
-  
 
-this._animate();
 
-this._controls.target.set(0.0, 2.0, 3.0);
+    }
 
-  
 
-this._animate();
 
-}
+    public init(orders: String[]): void {
 
-  
-  
+        this._car.setOrders(orders)
 
-public render(): void {
+        this._setUpLight();
 
-this._renderer.render(this._scene, this._camera);
+        this._setUpFloor();
 
-}
+        this._setUpBox();
 
-  
+        this._camera.position.set(0, 5, 5); // Position the camera above the plane
 
-public update(time: number): void {
+        this._camera.lookAt(0, 0, 0); // Look at the center of the plane
 
-// this._testObject.rotation.x += 0.01;
 
-// this._testObject.rotation.y += 0.01;
 
-this._car.update(time)
+        this._controls.target.set(0, 0, 0); // Set the target for the controls to the center of the plane
 
-  
 
-// // this._testObject.position.copy(this._mousePointInWorld);
 
-}
 
-  
+        this._animate();
 
-public getMousePosInWorldSpace(): THREE.Vector3 {
+        this._controls.target.set(0.0, 2.0, 3.0);
 
-const rayCaster = new THREE.Raycaster();
 
-rayCaster.setFromCamera(new THREE.Vector2(this._mousePosX, this._mousePosY), this._camera);
 
-  
+        this._animate();
 
-const intersections = rayCaster.intersectObject(this._planeOfIntersection, true);
+    }
 
-if (intersections.length > 0) {
 
-this._mousePointInWorld = intersections[0].point;
 
-this._isValidIntersection = true;
 
-return new THREE.Vector3(intersections[0].point.x, 1.0, intersections[0].point.z);
+    public render(): void {
 
-} else {
+        this._renderer.render(this._scene, this._camera);
 
-this._isValidIntersection = false;
+    }
 
-console.warn('No intersection found with the plane.');
 
-return new THREE.Vector3();
 
-}
+    public update(time: number): void {
 
-}
+        // this._testObject.rotation.x += 0.01;
 
-  
+        // this._testObject.rotation.y += 0.01;
 
-private _animate = (time: number): void => {
+        this._car.update(time)
 
-requestAnimationFrame(this._animate);
 
-this.update(time);
 
-this.render();
+        // // this._testObject.position.copy(this._mousePointInWorld);
 
-}
+    }
 
-  
 
-private _setUpFloor() {
 
-  
+    public getMousePosInWorldSpace(): THREE.Vector3 {
 
-const loadingManager = new THREE.LoadingManager();
+        const rayCaster = new THREE.Raycaster();
 
-  
+        rayCaster.setFromCamera(new THREE.Vector2(this._mousePosX, this._mousePosY), this._camera);
 
-const textureLoader = new THREE.TextureLoader(loadingManager);
 
-  
 
-const colorTexture = textureLoader.load('/textures/gravel_stones_diff_2k.jpg')
+        const intersections = rayCaster.intersectObject(this._planeOfIntersection, true);
 
-const ambientOclusion = textureLoader.load('/textures/gravel_stones_ao_2k.jpg')
+        if (intersections.length > 0) {
 
-const normalTexture = textureLoader.load('/textures/gravel_stones_nor_gl_2k.jpg')
+            this._mousePointInWorld = intersections[0].point;
 
-const roughtTexture = textureLoader.load('/textures/gravel_stones_rough_2k.jpg')
+            this._isValidIntersection = true;
 
-  
+            return new THREE.Vector3(intersections[0].point.x, 1.0, intersections[0].point.z);
 
-colorTexture.colorSpace = THREE.SRGBColorSpace;
+        } else {
 
-colorTexture.wrapS = THREE.MirroredRepeatWrapping
+            this._isValidIntersection = false;
 
-colorTexture.wrapT = THREE.MirroredRepeatWrapping
+            console.warn('No intersection found with the plane.');
 
-colorTexture.generateMipmaps = false
+            return new THREE.Vector3();
 
-colorTexture.minFilter = THREE.NearestFilter
+        }
 
-colorTexture.magFilter = THREE.NearestFilter
+    }
 
-  
 
-const geometry = new THREE.PlaneGeometry(10, 10);
 
-const material = new THREE.MeshPhysicalMaterial({ map: colorTexture });
+    private _animate = (time: number): void => {
 
-  
+        requestAnimationFrame(this._animate);
 
-material.aoMap = ambientOclusion;
+        this.update(time);
 
-material.roughnessMap = roughtTexture;
+        this.render();
 
-material.normalMap = normalTexture;
+    }
 
-  
 
-const planeMesh = new THREE.Mesh(geometry, material);
 
-planeMesh.rotation.x = -Math.PI / 2;
+    private _setUpFloor() {
 
-  
+        const loadingManager = new THREE.LoadingManager();
 
-this._scene.add(planeMesh);
+        const textureLoader = new THREE.TextureLoader(loadingManager);
 
-  
+        const colorTexture = textureLoader.load('/textures/gravel_stones_diff_2k.jpg')
+        const ambientOclusion = textureLoader.load('/textures/gravel_stones_ao_2k.jpg')
+        const normalTexture = textureLoader.load('/textures/gravel_stones_nor_gl_2k.jpg')
+        const roughtTexture = textureLoader.load('/textures/gravel_stones_rough_2k.jpg')
 
-this._planeOfIntersection = planeMesh;
+        colorTexture.colorSpace = THREE.SRGBColorSpace;
+        colorTexture.wrapS = THREE.MirroredRepeatWrapping
+        colorTexture.wrapT = THREE.MirroredRepeatWrapping
+        colorTexture.generateMipmaps = false
+        colorTexture.minFilter = THREE.NearestFilter
+        colorTexture.magFilter = THREE.NearestFilter
 
-}
+        const geometry = new THREE.PlaneGeometry(10, 10);
+        const material = new THREE.MeshPhysicalMaterial({ map: colorTexture, roughnessMap: roughtTexture, normalMap:normalTexture, aoMap: ambientOclusion  });
+        const planeMesh = new THREE.Mesh(geometry, material);
+        planeMesh.rotation.x = -Math.PI / 2;
+        planeMesh.receiveShadow = true;
+        planeMesh.castShadow = true;
+        this._planeOfIntersection = planeMesh;
+     
+        this._scene.add(planeMesh);
+    }
 
-  
 
-private _setUpLight() {
 
-const ambientLight = new THREE.AmbientLight();
+    private _setUpLight() {
 
-const directionalLight = new THREE.DirectionalLight(THREE.Color.NAMES.white, 3);
+        const ambientLight = new THREE.AmbientLight(THREE.Color.NAMES.white, 0.2);
+        const directionalLight = new THREE.DirectionalLight(THREE.Color.NAMES.white, 4);
+        directionalLight.position.set(12, 10, 0);
+        directionalLight.castShadow = true;
 
-directionalLight.position.set(0, 2, 0);
+        directionalLight.shadow.camera.near = 2;
+        directionalLight.shadow.camera.far = 50;
+        directionalLight.shadow.camera.left = -10;
+        directionalLight.shadow.camera.right = 10;
+        directionalLight.shadow.camera.top = 10;
+        directionalLight.shadow.camera.bottom = -10;
 
-this._scene.add(directionalLight);
+        directionalLight.shadow.camera.scale.set(2, 2, 2);
+        const helper = new THREE.CameraHelper(directionalLight.shadow.camera);
 
-}
+        //this._scene.add( helper );  
+        this._scene.add(ambientLight);
+        this._scene.add(directionalLight);
 
-  
+    }
 
-private _processMouseMove(event: any): void {
 
-this._mousePosX =
 
-((event.clientX - this._renderer.domElement.offsetLeft) /
+    private _processMouseMove(event: any): void {
 
-this._renderer.domElement.clientWidth) * 2 - 1;
+        this._mousePosX =
 
-  
+            ((event.clientX - this._renderer.domElement.offsetLeft) /
 
-this._mousePosY =
+                this._renderer.domElement.clientWidth) * 2 - 1;
 
-((event.clientY - this._renderer.domElement.offsetTop) /
 
-this._renderer.domElement.clientHeight) *
 
--2 +
+        this._mousePosY =
 
-1;
+            ((event.clientY - this._renderer.domElement.offsetTop) /
 
-  
+                this._renderer.domElement.clientHeight) *
 
-this.getMousePosInWorldSpace();
+            -2 +
 
-if (this._isValidIntersection)
+            1;
 
-this._pathConstructor.processMouseMove(this._mousePointInWorld);
 
-}
 
-  
+        this.getMousePosInWorldSpace();
 
-private _processMouseClick(event: any): void {
+        if (this._isValidIntersection)
 
-console.log("click")
+            this._pathConstructor.processMouseMove(this._mousePointInWorld);
 
-if (this._isValidIntersection)
+    }
 
-this._pathConstructor.processClickEvent(this._mousePointInWorld);
 
-}
 
-  
+    private _processMouseClick(event: any): void {
 
-private _start() {
+        console.log("click")
 
-console.log("start");
+        if (this._isValidIntersection)
 
-}
+            this._pathConstructor.processClickEvent(this._mousePointInWorld);
 
-  
+    }
 
-private _setUpBox() {
 
-const box = new THREE.BoxGeometry(1, 1, 1);
 
-const mat = new THREE.MeshPhysicalMaterial();
+    private _start() {
 
-// this._testObject = new THREE.Mesh(box, mat);
+        console.log("start");
 
-  
+    }
 
-// this._scene.add(this._testObject);
 
-}
+
+    private _setUpBox() {
+
+        const box = new THREE.BoxGeometry(1, 1, 1);
+
+        const mat = new THREE.MeshPhysicalMaterial();
+
+        // this._testObject = new THREE.Mesh(box, mat);
+
+
+
+        // this._scene.add(this._testObject);
+
+    }
 
 }
